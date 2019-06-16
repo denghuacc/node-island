2b12d45f04ed6a0d7d73c7eaa77d7bf7f22478eb
Hale Deng <haledeng88@gmail.com>
2019-06-16 22:54:45 +0800

    feat: add static serve

--- app.js
+++ app.js
@@ -2,15 +2,17 @@
  * @Author: Hale
  * @Description: 程序主入口
  * @Date: 2019-05-17
- * @LastEditTime: 2019-05-19
+ * @LastEditTime: 2019/06/16
  */
 const Koa = require('koa')
+const path = require('path')
+const serve = require('koa-static')
 const bodyParser = require('koa-bodyparser')
 const InitManager = require('./core/init')
 const catchError = require('./middlewares/exception')
 
 const app = new Koa()
-
+app.use(serve(path.resolve(__dirname, 'static')))
 app.use(catchError)
 app.use(bodyParser())
 InitManager.initCore(app)
